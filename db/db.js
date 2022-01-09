a6afb02bdac9ea8fdb20be7d2340697e60d733d5
el bandingo <elbandingo@protonmail.com>
2022-01-09 15:45:17 -0330

    delete note function now working in my routes. i added the delete function directly to my API routing file, so that i can keep my server file easy to read

--- db/db.js
+++ db/db.js
@@ -1,5 +1,1 @@
-//declare an array  to handle the notes, and export them for future editing/saving/deleting. each one will have an ID number incremented based on its position in the array.
-const notes = [
-];
-
-module.exports = notes;
+[]
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 //require the database of notes as a variable, and also FS for deleting and saving the new notes
 const notes = require("../db/db.js");
+const fs = require("fs");
+const path = require("path");
 
 
 //export a function to set the get and post routes
@@ -14,6 +16,25 @@ module.exports = function(app) {
         //create a new ID where its value is determined by its place in the notes array
         req.body.id = notes.length.toString();
         notes.push(req.body);
+        res.json(true);
+    });
+
+    //create a function that allows you to delete a note based on its ID
+    function deleteNote(id,noteArr) {
+        for (let i = 0; i <noteArr.length; i++) {
+            let currentNote = noteArr[i];
+
+            if (currentNote.id == id) {
+                noteArr.splice(i,1);
+                fs.writeFileSync(path.join(__dirname, '../db/db.js'),JSON.stringify(noteArr,null,2));
+                break;
+            }
+        }
+    }
+
+    //create a route to delete the note and reload the notes
+    app.delete("/api/notes/:id", (req,res) => {
+        deleteNote(req.params.id, notes);
         res.json(true);
     });
 
