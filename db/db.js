1820135ba6a6bb722e91c375a63880cc363b05c8
el bandingo <elbandingo@protonmail.com>
2022-01-09 15:36:40 -0330

    functions working, i cannot get a delete function to work, deploying to heroku, and will work on delete note options

--- db/db.js
+++ db/db.js
@@ -1,5 +1,4 @@
 //declare an array  to handle the notes, and export them for future editing/saving/deleting. each one will have an ID number incremented based on its position in the array.
-
 const notes = [
 ];
 
